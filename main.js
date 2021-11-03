5df859a46948f4bb9a30321d3f4c9575376e55b1
KAYQUE MOREIRA <56653725+Kaymoreira@users.noreply.github.com>
2021-11-03 16:20:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,135 @@
+// DOM -> Document Object Model
+
+//  abre e fecha o menu quando clicar no icone: hamburguer e X
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// quando clicar em um item do menu, esconder o menu
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* CHANGE HEADER WHEN SCROLL */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    //  scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //  menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/* Button back to top */
+
+function backToTopButton() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (this.window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*  Testimonials carousel slider swiper */
+
+const swiper = new Swiper('.swiper', {
+  slidesPerview: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+window.sr = ScrollReveal()
+
+sr.reveal(
+  `
+    #home .image, #home .text, 
+    #about .image, #about .text, #about .text
+    #services .header, #services .card, #services .title,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,
+  {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-50px',
+    reset: true
+  }
+)
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+
+/* const sr = ScrollReveal({
+  distance: '-50px',
+  duration: 700,
+  reset: true
+})
+
+sr.reveal(
+  `
+#home .image, #home .text, 
+#about .image, #about .text, #about .text
+#services .header, #services .card, #services .title,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links,
+footer .brand, footer .social
+`,
+  { interval: 50 }
+) */
+
+/* Menu ativo conforme a secao visivel na pagina */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+/* When scroll */
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTopButton()
+  activateMenuAtCurrentSection()
+})
